0f245efff8a921d59384eec53e637b913ecf2c25
Krzysztof Kowalski <kmkovalski@gmail.com>
2015-06-20 15:12:52 +0100

    Spring Boot App

---
+++ src/main/java/flanr/FlanrApplication.java
@@ -0,0 +1,12 @@
+package flanr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlanrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlanrApplication.class, args);
+    }
+}
